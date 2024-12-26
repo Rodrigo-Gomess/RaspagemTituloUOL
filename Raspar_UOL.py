74bc9ada3dcbeeea923313d3e94f52f80cfff9c4
Rodrigo <rodrigodigon23@gmail.com>
2024-12-25 21:42:55 -0300

    Aprimorado para utilizar SQLite,

--- Raspar_UOL.py
+++ Raspar_UOL.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
 import sqlite3
-import schedule
-import time
+
+
 
 def cria_tabela():
     # Conectar ao banco de dados (ou criar se não existir)
@@ -22,6 +22,7 @@ def cria_tabela():
     conn.commit()
     conn.close()
 
+
 def insere_titulo(titulo_texto, data_coleta):
     # Conectar ao banco de dados
     conn = sqlite3.connect('titulos_uol.db')
@@ -36,29 +37,26 @@ def insere_titulo(titulo_texto, data_coleta):
     conn.close()
 
 
-url = 'https://uol.com.br'
+def coleta_titulo():
+    url = 'https://uol.com.br'
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Armazena o primeiro título na variável titulos
-titulo = soup.find('h3', 'headlineMain__title')
+    # Armazena o primeiro título na variável titulos
+    titulo = soup.find('h3', 'headlineMain__title')
 
-# Verifica se encontrou o título desejado
-if titulo:
-    titulo_texto = titulo.get_text().strip()
-    print(titulo_texto)
+    # Verifica se encontrou o título desejado
+    if titulo:
+        titulo_texto = titulo.get_text().strip()
+        print(titulo_texto)
 
-    #Data da coleta
-    data_coleta = datetime.now().strftime('%Y-%m-%d %H:%M') # Formato ano-mes-dia hora-minuto
+        #Data da coleta
+        data_coleta = datetime.now().strftime('%Y-%m-%d %H:%M')  # Formato ano-mes-dia hora-minuto
 
-    # Armazenando em um DataFrame
-    df = pd.DataFrame({
-        'Titulo': [titulo_texto],
-        'Data': [data_coleta]})
+        # insere os dados na tabela SQLite
+        insere_titulo(titulo_texto, data_coleta)
 
 
-    # Salvando o DataFrame em um arquivo CSV
-    df.to_csv('titulos_uol.csv', mode='a', header=not pd.io.common.file_exists('titulos_uol.csv'), index=False)
-else:
-    print('Algo deu errado com a extração')
+cria_tabela()
+coleta_titulo()
